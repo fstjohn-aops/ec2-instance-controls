79a462b4bd52a317d6f3c34d3d367530e294b305
Finn St John <fstjohn@artofproblemsolving.com>
2025-06-26 13:45:52 -0700

    Change file permissions for `push-image.sh` to make it executable.

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source .venv/bin/activate
+pip install -r requirements.txt
+python3 test_app.py
+deactivate
---
+++ test_app.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import pytest
+import sys
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["test/", "-v"])) 
